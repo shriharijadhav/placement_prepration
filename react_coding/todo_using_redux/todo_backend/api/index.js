a2971bc7682d262444ba9dfa22578b73af6dc6b3
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-07 08:35:03 +0530

    Done-verceldemo

---
+++ react_coding/todo_using_redux/todo_backend/api/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+dotenv.config()
+
+
+
+const todoRouter = require('../routes/todo.route') 
+
+
+
+const dbConnect = require('../config/dbConnect')
+dbConnect();
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use(todoRouter)
+
+
+app.listen(PORT,()=>{
+    console.log('server listening on port'+PORT)
+})
